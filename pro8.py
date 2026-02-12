425127cd27fbc4008ea020878cadc81ca63ec01a
sneha-latha-2004 <sneha22ec052@drttit.edu.in>
2026-02-12 19:21:36 +0530

    pro8

---
+++ pro8.py
@@ -0,0 +1,11 @@
+day = int(input("Enter day number (1-3): "))
+
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case _:
+        print("Invalid day")
